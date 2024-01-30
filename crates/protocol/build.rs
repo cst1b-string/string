d8ac4c64b988b7a027200d4459e7493d42bd2f95
kaylen ✨🍓 <actuallyori@gmail.com>
2024-01-30 18:54:29 +0000

    feat(kaylendog/protocol): improved buf generation; basic setup

--- crates/protocol/build.rs
+++ crates/protocol/build.rs
@@ -1,5 +1,14 @@
-use std::io::Result;
-fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/users.proto"], &["src/"])?;
+fn main() -> std::io::Result<()> {
+    // rebuild rust definitions if base changed
+    println!("cargo:rerun-if-changed=string/**/*.proto");
+
+    // find all protocol files
+    let files = glob::glob("./string/**/*.proto")
+        .expect("failed to find protocol files")
+        .collect::<Result<Vec<_>, _>>()
+        .expect("failed to find protocol files");
+
+    // and compile them
+    prost_build::compile_protos(&files, &["string/"])?;
     Ok(())
 }
--- crates/protocol/src/lib.rs
+++ crates/protocol/src/lib.rs
@@ -2,7 +2,29 @@
 //!
 //! This crate contains the protocol definition for the string protocol.
 
-/// Defines user types for the protocol.
+/// Utility macro to quickly define a module for a protocol.
+macro_rules! include_protocol {
+    ($name:literal, $version:ident) => {
+        #[doc=concat!("Documentation for version", stringify!($version), "of the", $name, "protocol.")]
+        pub mod $version {
+            include!(concat!(
+                env!("OUT_DIR"),
+                "/string.",
+                $name,
+                ".",
+                stringify!($version),
+                ".rs",
+            ));
+        }
+    };
+}
+
+/// Defines the user buffer types and data.
 pub mod users {
-    include!(concat!(env!("OUT_DIR"), "/string.users.rs"));
+    include_protocol!("users", v1);
+}
+
+/// Defines the messages buffer types and data.
+pub mod messages {
+    include_protocol!("messages", v1);
 }
