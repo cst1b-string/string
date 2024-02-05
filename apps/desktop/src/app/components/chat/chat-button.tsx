cdbe54175b2ab4bf14fce911f830850d85bfc8ee
David Murray <dm966@cam.ac.uk>
2024-02-05 12:23:07 +0000

    feat: template navbar

---
+++ apps/desktop/src/app/components/chat/chat-button.tsx
@@ -0,0 +1,7 @@
+export const ChatButton = () => {
+	return (
+		<button className="bg-white text-[#191970] px-4 py-2 rounded-md">
+			New Chat
+		</button>
+	);
+}
---
+++ apps/desktop/src/app/components/chat/index.tsx
@@ -0,0 +1,9 @@
+import { ChatButton } from "./chat-button";
+
+export const Chat = () => {
+	return (
+		<div>
+			< ChatButton />
+		</div>
+	);
+} 
