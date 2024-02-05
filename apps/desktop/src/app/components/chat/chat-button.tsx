fe414dabfb764769c173b9415b764c7d6462617e
David Murray <dm966@cam.ac.uk>
2024-02-05 16:00:29 +0000

    feat: background colour and chat button

--- apps/desktop/src/app/components/chat/chat-button.tsx
+++ apps/desktop/src/app/components/chat/chat-button.tsx
@@ -1,7 +1,7 @@
 export const ChatButton = () => {
 	return (
-		<button className="bg-white text-[#191970] px-4 py-2 rounded-md">
-			New Chat
+		<button className="bg-[#3399FF] text-[white] w-full py-4">
+			New Chat +
 		</button>
 	);
 }
