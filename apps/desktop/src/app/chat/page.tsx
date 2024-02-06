091c3d91d27fceea355774a211551aa11fc2d0fb
S Forbes <sf729@cam.ac.uk>
2024-02-06 11:58:14 +0000

    feat: file structure set up

---
+++ apps/desktop/src/app/chat/page.tsx
@@ -0,0 +1,10 @@
+//import { Chat } from '../components/chat';
+import { Navbar } from '../components/navbar';
+
+export default function Home() {
+	return (
+		<div>
+			<Navbar />
+		</div>
+	); //Amaan
+}
---
+++ apps/desktop/src/app/settings/page.tsx
@@ -0,0 +1,11 @@
+//import { Chat } from '../components/chat';
+import { Navbar } from '../components/navbar';
+
+export default function Home() {
+	return (
+		<div>
+			<Navbar />
+			<h1> "Hello world" </h1>
+		</div>
+	);
+}
