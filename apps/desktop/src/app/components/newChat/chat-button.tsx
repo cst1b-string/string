567a8607bed28aceec03b7aaf8774b9e7a0323fe
David Murray <dm966@cam.ac.uk>
2024-02-06 12:17:26 +0000

    feat: file structure

--- apps/desktop/src/app/components/newChat/chat-button.tsx
+++ apps/desktop/src/app/components/newChat/chat-button.tsx
@@ -1,12 +1,7 @@
-import Link from 'next/link';
-
 export const ChatButton = () => {
 	return (
-		
-			<Link href="/chat"> 
-				<button className="bg-[#3399FF] text-[white] w-full py-4"> 
-				New Chat +  
-				</button>
-			</Link>
+		<button className="bg-[#3399FF] text-[white] w-full py-4">
+			New Chat +
+		</button>
 	);
 }
--- apps/desktop/src/app/page.tsx
+++ apps/desktop/src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Chat } from './components/newChat';
+import { Navbar } from './components/navbar';
 
 export default function Home() {
 	return (
 		<div>
+<<<<<<< HEAD
 
 			<Chat />
+=======
+			< Navbar />
+			< Chat />
+>>>>>>> 72d3478 (feat: file structure)
 		</div>
 	);
 }
