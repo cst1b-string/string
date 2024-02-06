c7ff6672fd73e1d60640ef3662e1725a60ed8dbc
S Forbes <sf729@cam.ac.uk>
2024-02-06 12:32:53 +0000

    feat: logo now links back to home page

--- apps/desktop/src/app/components/navbar/logo.tsx
+++ apps/desktop/src/app/components/navbar/logo.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 export const Logo = () => {
 	return (
 		<div className="flex items-center gap-x-2">
-			<h1 className="text-white text-2xl font-bold">String</h1>
+			<Link href=".">
+				<h1 className="text-white text-2xl font-bold">String</h1>
+			</Link>
 		</div>
 	);
 }
