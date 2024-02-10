ea6adffb518a84c1672fb628eef12b80f10f244b
kaylen ✨🍓 <actuallyori@gmail.com>
2024-02-10 15:06:56 +0000

    feat(kaylendog/sequence): use clap for comm-test; tracing

--- tools/comm-test/src/main.rs
+++ tools/comm-test/src/main.rs
@@ -1,50 +1,75 @@
-use std::{net::IpAddr, net::Ipv4Addr, net::SocketAddr, time::Duration};
+use clap::Parser;
 use comm::{peer::PeerState, Socket};
-use std::env;
+use std::{net::SocketAddr, time::Duration};
+use tracing::{error, info, level_filters::LevelFilter};
+use tracing_subscriber::EnvFilter;
+
+/// comm-test is a simple tool to test the string-comm crate.
+#[derive(Debug, Parser)]
+struct Args {
+    /// The source port to bind to.
+    bind_port: u16,
+    /// The destination IP address to add as a peer.
+    peer_addr: SocketAddr,
+    /// Whether to initiate the connection.
+    #[clap(long)]
+    initiate: bool,
+}
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 5 {
-        println!("Usage: {0} srcport dstip dstport <true/false>", args[0]);
-    }
-    else {
-        let peer_addr = SocketAddr::new(
-            IpAddr::V4(args[2].parse::<Ipv4Addr>().expect("Bad IP")),
-            args[3].parse::<u16>().expect("Bad dstport")
-        );
-        let sock_addr = SocketAddr::new(
-            IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
-            args[1].parse::<u16>().expect("Bad srcport")
-        );
-        let mut sock = match Socket::bind(sock_addr).await {
-            Ok(s) => s,
-            Err(_) => {
-                eprintln!("[-] Failed to bind to local.");
-                return;
-            }
-        };
-        let initiate = args[4].parse::<bool>().expect("Bad initiate");
-        let (_app_outbound_tx, _app_inbound_rx) = sock.add_peer(peer_addr, initiate).await;
-        println!("[+] Setup success!");
-        println!("[*] Attempting transmission...");
-        let mut i: u16 = 0;
-        let mut ready: bool = false;
-        // Wait 5 mins
-        while i < 5 * 60 * 2 && !ready {
-            match sock.get_peer_state(peer_addr).await {
-                None => {},
-                Some(s) => {
-                    if s == PeerState::Established { ready = true; }
-                }
-            }
-            i += 1;
-            tokio::time::sleep(Duration::from_millis(500)).await;
-        }
-        if !ready {
-            eprintln!("[-] Connection failure.");
+    let Args {
+        bind_port,
+        initiate,
+        peer_addr,
+    } = Args::parse();
+
+    // initialise tracing
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::TRACE.into())
+                .from_env_lossy(),
+        )
+        .init();
+
+    // bind to the socket
+    let mut socket = match Socket::bind(([0, 0, 0, 0], bind_port).into()).await {
+        Ok(s) => s,
+        Err(_) => {
+            error!("[-] Failed to bind to local.");
             return;
         }
-        println!("[+] Connection with {0} succeeded!", peer_addr);
+    };
+
+    // add peer
+    let (_app_outbound_tx, _app_inbound_rx) = socket.add_peer(peer_addr, initiate).await;
+
+    info!("Setup success");
+    info!("Attempting transmission...");
+
+    let mut i: u16 = 0;
+    let mut ready: bool = false;
+
+    // Wait 5 mins
+    while i < 5 * 60 * 2 && !ready {
+        match socket.get_peer_state(peer_addr).await {
+            None => {}
+            Some(s) => {
+                if s == PeerState::Established {
+                    ready = true;
+                }
+            }
+        }
+        i += 1;
+        tokio::time::sleep(Duration::from_millis(500)).await;
     }
+
+    // check if the connection is ready
+    if !ready {
+        error!("[-] Connection failure.");
+        return;
+    }
+
+    info!("[+] Connection with {0} succeeded!", peer_addr);
 }
