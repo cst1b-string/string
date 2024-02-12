b166693203402dd31b7a9a4a613cddd70cb5e4ba
David Murray <dm966@cam.ac.uk>
2024-02-12 10:57:23 +0000

    feat: moved components from src/app to src

--- apps/desktop/src/app/page.tsx
+++ apps/desktop/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { ExistingChat } from './components/existingChat';
-import { NewChat } from './components/newChat';
+import { ExistingChat } from '../components/existingChat';
+import { NewChat } from '../components/newChat';
 
 export default function Home() {
 	return (
