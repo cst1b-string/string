542208a2b58ae60f6fa1a1c74a0c32d99150669b
David Murray <dm966@cam.ac.uk>
2024-02-18 15:00:30 +0000

    feat: sign in form initial design (non-functional)

--- apps/desktop/src/app/signIn/page.tsx
+++ apps/desktop/src/app/signIn/page.tsx
@@ -1,39 +1,30 @@
-'use client'
-
-import React, { useState } from 'react';
 
 export default function SignIn() {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-
-	const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setUsername(event.target.value);
-	};
-
-	const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setPassword(event.target.value);
-	};
-
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		// Perform sign-in logic here
-	};
 
 	return (
-		<div>
-			<form onSubmit={handleSubmit}>
+		<div className="py-6 flex justify-center">
+		<div className="bg-white rounded px-12 py-10 flex flex-col space-y-4 w-96">
+			<h1 className="text-2xl">Sign In</h1>
+			<form className="flex flex-col space-y-6">
 				<label>
-					Username:
-					<input type="text" value={username} onChange={handleUsernameChange} />
+					Username
+					<br />
+					<input type="text" className="py-1 px-1 rounded border border-slate-500 w-full"/>
+				</label>
+				<label className="flow-root">
+					Password
+					<br />
+					<input type="password" className="py-1 px-1 rounded border border-slate-500 w-full"/>
+					<a href="/reset-password" className="text-blue-400 float-right text-sm">Forgot Password?</a>
 				</label>
 				<br />
-				<label>
-					Password:
-					<input type="password" value={password} onChange={handlePasswordChange} />
-				</label>
+				<button type="submit" className="py-2 rounded drop-shadow-lg bg-blue-400 text-white">Sign In</button>
 				<br />
-				<button type="submit">Sign In</button>
+				<div className="text-center">
+					Need an account? <a href="/sign-up" className="text-blue-400">Sign Up</a>
+				</div>
 			</form>
+		</div>	
 		</div>
 	);
 }
