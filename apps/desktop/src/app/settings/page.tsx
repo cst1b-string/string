c97a6c0202ca287fa238d650e587be06cfd03304
S Forbes <sf729@cam.ac.uk>
2024-02-29 11:50:17 +0000

    feat: failed tests for buttons (feature not a bug)

--- apps/desktop/src/app/settings/page.tsx
+++ apps/desktop/src/app/settings/page.tsx
@@ -9,29 +9,28 @@
 import { useContext, useState } from 'react';
 import { themeContext } from '../page';
 
-const [usernameState, setUsernameState] = useState('');
+export function set_username(username: string) {console.log(username)} //placeholder
 
-export function set_username(username: string) {} //placeholder
-
-export function handleUsernameSubmit() {
+export function handleUsernameSubmit(usernameState: string) {
 	set_username(usernameState);
 }
 
-export function handleUsernameChange(e: React.FormEvent<HTMLInputElement>) {
+export function handleUsernameChange(e: React.FormEvent<HTMLInputElement>, setUsernameState: React.Dispatch<React.SetStateAction<string>>) {
 
+	console.log("test")
 	setUsernameState(e.currentTarget.value);
 
 }
 
 export function set_bio(bio: string) {} //placeholder
 
-export function handleBioSubmit() {
-	set_username(usernameState);
+export function handleBioSubmit(bioState: string) {
+	set_bio(bioState);
 }
 
-export function handleBioChange(e: React.FormEvent<HTMLTextAreaElement>) {
+export function handleBioChange(e: React.FormEvent<HTMLTextAreaElement>, setBioState: React.Dispatch<React.SetStateAction<string>>) {
 
-	setUsernameState(e.currentTarget.value);
+	setBioState(e.currentTarget.value);
 
 }
 
@@ -46,8 +45,6 @@ export default function Home() {
 	var lightModeText;
 	var Username = "<Username from Tauri>";
 
-	setUsernameState(Username);
-
 	var Bio = "<Bio from Tauri>";
 	if (lightMode) {
 		lightModeText = "Dark Mode"
@@ -56,6 +53,10 @@ export default function Home() {
 	else {
 		lightModeText = "Light Mode"
 	}
+
+	const [usernameState, setUsernameState] = useState(Username);
+	const [bioState, setBioState] = useState(Bio);
+
 	return (
 		<div className="flex justify-center">
 			<div className="py-5 w-1/3 space-y-1">
@@ -70,9 +71,9 @@ export default function Home() {
 				<div className="flex justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md">
 
 					<h1 className="px-1"> Username: </h1>
-					<input className="bg-[#113355] px-1" defaultValue={Username} onChange={handleUsernameChange}/>
+					<input className="bg-[#113355] px-1" defaultValue={Username} onChange={(e) => handleUsernameChange(e, setUsernameState)}/>
 					
-					<button className="px-1 bg-black rounded-md" onClick={handleUsernameSubmit}>
+					<button className="px-1 bg-black rounded-md" onClick={() => (handleUsernameSubmit(usernameState))}>
 						<h1> Submit </h1>
 					</button>
 
@@ -81,9 +82,9 @@ export default function Home() {
 				<div className="flex justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md">
 
 					<h1 className="px-1"> User Bio: </h1>
-					<textarea className="bg-[#113355] px-1 resize-none" rows={5} defaultValue={Bio} onChange={handleBioChange}/>
+					<textarea className="bg-[#113355] px-1 resize-none" rows={5} defaultValue={Bio} onChange={(e) => handleBioChange(e, setBioState)}/>
 
-					<button className="px-1 py-5 bg-black rounded-md" onClick={() => false}>
+					<button className="px-1 py-5 bg-black rounded-md" onClick={() => (handleBioSubmit(bioState))}>
 						<h1> Submit </h1>
 					</button>
 
