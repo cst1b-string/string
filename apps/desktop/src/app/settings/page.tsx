051561f39969f4b2ab9f6b04e345dc569f06531a
kaylen ✨🍓 <actuallyori@gmail.com>
2024-02-29 12:55:14 +0000

    fix(kaylendog/settings): fixup theme toggle button

--- apps/desktop/src/app/settings/page.tsx
+++ apps/desktop/src/app/settings/page.tsx
@@ -2,10 +2,10 @@
 
 import { useContext, useMemo, useState } from "react";
 
-import { themeContext } from "../page";
+import { ThemeContext } from "../layout";
 
 export default function Home() {
-	const { lightMode, setLightMode } = useContext(themeContext);
+	const { lightMode, setLightMode } = useContext(ThemeContext);
 
 	const lightModeText = useMemo(() => (lightMode ? "Dark Mode" : "Light Mode"), [lightMode]);
 
@@ -16,9 +16,7 @@ export default function Home() {
 		<div className="flex justify-center">
 			<div className="py-5 w-1/3 space-y-1">
 				<div className="flex justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md">
-					<button onClick={() => setLightMode(!lightMode)}>
-						<h1> {lightModeText} </h1>
-					</button>
+					<button onClick={() => setLightMode(!lightMode)}>{lightModeText}</button>
 				</div>
 
 				<form
@@ -28,14 +26,14 @@ export default function Home() {
 						console.log("do some calls to the backend here!", username, bio);
 					}}
 				>
-					<h1 className="px-1"> Username: </h1>
+					<label className="px-1"> Username: </label>
 					<input
 						className="bg-[#113355] px-1"
 						value={username}
 						onChange={(e) => setUsername(e.currentTarget.value)}
 					/>
 
-					<h1 className="px-1"> User Bio: </h1>
+					<label className="px-1"> Username: </label>
 					<textarea
 						className="bg-[#113355] px-1 resize-none"
 						rows={5}
