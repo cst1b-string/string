29a4d311654d6ba8bf4966fd2745ea72c3f246ce
kaylen ✨🍓 <actuallyori@gmail.com>
2024-02-29 13:12:18 +0000

    feat(kaylendog/settings): fix stylings

--- apps/desktop/src/app/settings/page.tsx
+++ apps/desktop/src/app/settings/page.tsx
@@ -13,35 +13,44 @@ export default function Home() {
 	const [bio, setBio] = useState("<Bio from Tauri>");
 
 	return (
-		<div className="flex justify-center">
-			<div className="py-5 w-1/3 space-y-1">
-				<div className="flex justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md">
+		<div className="flex flex-row justify-center py-5 w-full ">
+			<div className="flex flex-col space-y-4 w-1/3">
+				<div className="flex  justify-center bg-[#335577] text-[white] px-4 py-2 rounded-md cursor-pointer hover:bg-[#224466]">
 					<button onClick={() => setLightMode(!lightMode)}>{lightModeText}</button>
 				</div>
 
 				<form
-					className="flex justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md"
+					className="flex justify-center flex-col space-y-4"
 					onSubmit={(e) => {
 						e.preventDefault();
 						console.log("do some calls to the backend here!", username, bio);
 					}}
 				>
-					<label className="px-1"> Username: </label>
+					<div className="flex items-center justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md">
+						<label className="mr-4"> Username</label>
+						<input
+							className="flex-grow bg-[#335577] p-4 rounded-md"
+							value={username}
+							onChange={(e) => setUsername(e.currentTarget.value)}
+						/>
+					</div>
+
+					<div className="flex flex-col justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md">
+						<label className="py-4"> Biography</label>
+						<textarea
+							className="p-4 resize-none w-full rounded-md bg-[#335577]"
+							rows={5}
+							value={bio}
+							onChange={(e) => setBio(e.target.value)}
+						/>
+					</div>
+
 					<input
-						className="bg-[#113355] px-1"
-						value={username}
-						onChange={(e) => setUsername(e.currentTarget.value)}
+						id="submit"
+						type="submit"
+						className="flex justify-center bg-[#335577] text-[white] px-4 py-2 rounded-md cursor-pointer hover:bg-[#224466]"
+						value="Submit"
 					/>
-
-					<label className="px-1"> Username: </label>
-					<textarea
-						className="bg-[#113355] px-1 resize-none"
-						rows={5}
-						value={bio}
-						onChange={(e) => setBio(e.target.value)}
-					/>
-
-					<input id="submit" type="submit" className="px-1 bg-black rounded-md" value="Submit" />
 				</form>
 			</div>
 		</div>
