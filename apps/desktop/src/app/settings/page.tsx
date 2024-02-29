5a201381bce07f1e1f9e9d2b76ff3b2ee4f17cf8
kaylen ✨🍓 <actuallyori@gmail.com>
2024-02-29 12:48:30 +0000

    feat(kaylendog/settings): fixup form submission

--- apps/desktop/src/app/settings/page.tsx
+++ apps/desktop/src/app/settings/page.tsx
@@ -1,95 +1,50 @@
-// Profile display name, bio?, dark/light mode, clear chats, etc.
+"use client";
 
-'use client'
+import { useContext, useMemo, useState } from "react";
 
-{/* <button onClick={() => false}>
-<h1> Change Username: {Username} </h1>
-</button> */}
-
-import { useContext, useState } from 'react';
-import { themeContext } from '../page';
-
-export function set_username(username: string) {console.log(username)} //placeholder
-
-export function handleUsernameSubmit(usernameState: string) {
-	set_username(usernameState);
-}
-
-export function handleUsernameChange(e: React.FormEvent<HTMLInputElement>, setUsernameState: React.Dispatch<React.SetStateAction<string>>) {
-
-	console.log("test")
-	setUsernameState(e.currentTarget.value);
-
-}
-
-export function set_bio(bio: string) {} //placeholder
-
-export function handleBioSubmit(bioState: string) {
-	set_bio(bioState);
-}
-
-export function handleBioChange(e: React.FormEvent<HTMLTextAreaElement>, setBioState: React.Dispatch<React.SetStateAction<string>>) {
-
-	setBioState(e.currentTarget.value);
-
-}
-
-export function toggleLightMode(lightMode: boolean) {
-	//backend call
-}
+import { themeContext } from "../page";
 
 export default function Home() {
+	const { lightMode, setLightMode } = useContext(themeContext);
 
-	const lightModeComposite = useContext(themeContext);
-	const lightMode = lightModeComposite.lightMode;
-	var lightModeText;
-	var Username = "<Username from Tauri>";
+	const lightModeText = useMemo(() => (lightMode ? "Dark Mode" : "Light Mode"), [lightMode]);
 
-	var Bio = "<Bio from Tauri>";
-	if (lightMode) {
-		lightModeText = "Dark Mode"
-	}
-
-	else {
-		lightModeText = "Light Mode"
-	}
-
-	const [usernameState, setUsernameState] = useState(Username);
-	const [bioState, setBioState] = useState(Bio);
+	const [username, setUsername] = useState("<Username from Tauri>");
+	const [bio, setBio] = useState("<Bio from Tauri>");
 
 	return (
 		<div className="flex justify-center">
 			<div className="py-5 w-1/3 space-y-1">
 				<div className="flex justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md">
-
-					<button onClick={() => toggleLightMode(!lightMode)}>
+					<button onClick={() => setLightMode(!lightMode)}>
 						<h1> {lightModeText} </h1>
 					</button>
-
 				</div>
 
-				<div className="flex justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md">
-
+				<form
+					className="flex justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md"
+					onSubmit={(e) => {
+						e.preventDefault();
+						console.log("do some calls to the backend here!", username, bio);
+					}}
+				>
 					<h1 className="px-1"> Username: </h1>
-					<input className="bg-[#113355] px-1" defaultValue={Username} onChange={(e) => handleUsernameChange(e, setUsernameState)}/>
-					
-					<button className="px-1 bg-black rounded-md" onClick={() => (handleUsernameSubmit(usernameState))}>
-						<h1> Submit </h1>
-					</button>
-
-				</div>
-
-				<div className="flex justify-center bg-[#113355] text-[white] px-4 py-2 rounded-md">
+					<input
+						className="bg-[#113355] px-1"
+						value={username}
+						onChange={(e) => setUsername(e.currentTarget.value)}
+					/>
 
 					<h1 className="px-1"> User Bio: </h1>
-					<textarea className="bg-[#113355] px-1 resize-none" rows={5} defaultValue={Bio} onChange={(e) => handleBioChange(e, setBioState)}/>
+					<textarea
+						className="bg-[#113355] px-1 resize-none"
+						rows={5}
+						value={bio}
+						onChange={(e) => setBio(e.target.value)}
+					/>
 
-					<button className="px-1 py-5 bg-black rounded-md" onClick={() => (handleBioSubmit(bioState))}>
-						<h1> Submit </h1>
-					</button>
-
-				</div>
-
+					<input id="submit" type="submit" className="px-1 bg-black rounded-md" value="Submit" />
+				</form>
 			</div>
 		</div>
 	);
