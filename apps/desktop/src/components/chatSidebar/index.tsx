4c1e0d0a0e828a119367af8ec246c90978c6aeb5
David Murray <dm966@cam.ac.uk>
2024-03-02 20:09:19 +0000

    feat: added mutable state into sidebar that can be shared with chat log

--- apps/desktop/src/components/chatSidebar/index.tsx
+++ apps/desktop/src/components/chatSidebar/index.tsx
@@ -1,26 +1,46 @@
 "use client";
 
 import { useRspc } from "@/integration";
+import { Channel } from "@/integration/bindings";
 import Link from "next/link";
 
-
-export default function ChatSidebar() {
-
+export default function ChatSidebar({
+	selectedChannel,
+	setSelectedChannel,
+}: {
+	selectedChannel: number;
+	setSelectedChannel: (channel: number) => void;
+}) {
 	const rspc = useRspc();
-	const { data } = rspc.useQuery(['channel.list']);
-	
+	const { data } = rspc.useQuery(["channel.list"]);
+
+	if (data) {
+		setSelectedChannel(data[0]!.id as number);
+	}
+
+	const handleChannelClick = (channel: Channel) => {
+		setSelectedChannel(channel.id as number);
+	};
+
 	return (
 		<div>
 			<Link href="./newChat">
 				<button className="bg-transparent hover:bg-[#4e627a] text-white w-full py-5 rounded">
-					"New Chat"
+					New Chat
 				</button>
 			</Link>
-			{data && data.map((channel) => (
-				<button className="bg-transparent hover:bg-[#4e627a] text-white w-full py-5 rounded">
-					{channel.title}
-				</button>
-			))}
+			{data &&
+				data.map((channel) => (
+					<button
+						key={channel.id}
+						className={`bg-transparent hover:bg-[#4e627a] text-white w-full py-5 rounded ${
+							selectedChannel === channel.id ? "bg-[#4e627a]" : ""
+						}`}
+						onClick={() => handleChannelClick(channel)}
+					>
+						{channel.title}
+					</button>
+				))}
 		</div>
 	);
 }
