b39eee5e4cf2afb6493457527cfd5d6f5f5b63a5
S Forbes <sf729@cam.ac.uk>
2024-03-04 16:59:46 +0000

    chore: renamed variables and added comments

--- apps/desktop/src/app/settings/page.tsx
+++ apps/desktop/src/app/settings/page.tsx
@@ -4,16 +4,16 @@ import { useRspc } from "@/integration";
 import { useEffect, useMemo, useState } from "react";
 
 export default function Home() {
-	const rspc = useRspc();
+	const rspc = useRspc(); 
 
-	const { data, refetch } = rspc.useQuery(["settings.theme"]);
-	const { mutate, isSuccess } = rspc.useMutation(["settings.theme"]);
+	const { data, refetch } = rspc.useQuery(["settings.theme"]); //To load from backend - data is theme "Light" / "Dark"
+	const { mutate: themeMutate, isSuccess:themeIsSuccess } = rspc.useMutation(["settings.theme"]); //To send updated theme to backend
 
 	useEffect(() => {
 		refetch();
-	}, [isSuccess]);
+	}, [themeIsSuccess]); //Refetch on successful backend call
 
-	const lightModeText = useMemo(() => (data === "Dark" ? "Dark Mode" : "Light Mode"), [data]);
+	const lightModeText = useMemo(() => (data === "Dark" ? "Dark Mode" : "Light Mode"), [data]); //Text to be displayed on theme button
 
 	const [username, setUsername] = useState("<Username from Tauri>");
 	const [bio, setBio] = useState("<Bio from Tauri>");
@@ -22,7 +22,7 @@ export default function Home() {
 		<div className="flex flex-row justify-center py-5">
 			<div className="flex flex-col space-y-4 w-[600px]">
 				<div className="flex  justify-center bg-[#335577] text-[white] px-4 py-2 rounded-md cursor-pointer hover:bg-[#224466]">
-					<button onClick={() => mutate(data === "Dark" ? "Light" : "Dark")}>
+					<button onClick={() => themeMutate(data === "Dark" ? "Light" : "Dark")}>
 						{lightModeText}
 					</button>
 				</div>
