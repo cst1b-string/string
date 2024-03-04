66060b17603adb30f2ef942d47cb3d71cec94b1c
David Murray <dm966@cam.ac.uk>
2024-03-04 18:33:15 +0000

    feat: gave colours names in tailwind config

--- apps/desktop/src/components/chatSidebar/index.tsx
+++ apps/desktop/src/components/chatSidebar/index.tsx
@@ -3,7 +3,6 @@
 import { CircularChatButton } from "@/components/circularChatButton";
 import { useRspc } from "@/integration";
 import { Channel } from "@/integration/bindings";
-import Link from "next/link";
 import { useState } from "react";
 
 export default function ChatSidebar({
@@ -62,22 +61,22 @@ export default function ChatSidebar({
 	};
 
 	return (
-		<div className={`h-full ${isExpanded ? "min-w-70" : "min-w-20"}`}>
-			<div className="px-2 flex flex-col items-center no-scrollbar overflow-auto space-y-1 bg-transparent h-full py-2">
-				<div className={`${isExpanded ? "grid grid-cols-5" : "flex justify-center"} space-x-1`}>
+		<div className={`bg-darkSidebar h-full ${isExpanded ? "min-w-70" : "min-w-20"} grid grid-rows-[auto,1fr,auto]`}>
+			<div className={`${isExpanded ? "grid grid-cols-5" : "flex justify-center"} space-x-1 h-[60px] py-2 px-2`}>
 					<input
 						id="search"
 						type="text"
 						placeholder="Search"
 						className={`px-1 py-1 rounded ${
 							isExpanded ? "" : "hidden"
-						} bg-buttonBlue text-white col-span-4`}
+						} bg-darkInput text-darkText col-span-4`}
 						onChange={handleSearch}
 					/>
-					<button onClick={toggleExpansion} className="mt-auto mb-2 text-white font-bold text-2xl">
+					<button onClick={toggleExpansion} className="mt-auto mb-2 text-darkText font-bold text-2xl">
 						{isExpanded ? "-" : "+"}
 					</button>
 				</div>
+			<div className="px-2 py-2 flex flex-col items-center no-scrollbar overflow-auto space-y-1 bg-transparent">
 				{data &&
 					data.map((channel) => (
 						<div
@@ -86,15 +85,17 @@ export default function ChatSidebar({
 								filteredChannels.has(channel.id) ? "hidden" : ""
 							} ${
 								selectedChannel == channel.id
-									? "bg-buttonBlue"
-									: "bg-transparent hover:bg-hoverBlue"
+									? "bg-darkSelected"
+									: " hover:bg-darkHover"
 							}`}
 							onClick={() => handleChannelClick(channel)}
 						>
 							<CircularChatButton chatName={channel.title} isExpanded={isExpanded} />
 						</div>
-					))}
-				<div className={`flex flex-col pt-2 ${align}`}>
+					))}	
+			</div>
+			<div className={`bg-darkNewChat flex justify-center px-2 py-2 ${align}`}>
+				<div className="w-full py-1 px-1 rounded hover:bg-darkHover">
 					<CircularChatButton chatName="+" isExpanded={isExpanded} />
 				</div>
 			</div>
--- apps/desktop/src/components/navbar/index.tsx
+++ apps/desktop/src/components/navbar/index.tsx
@@ -3,7 +3,7 @@ import { SettingsButton } from "./settings-button";
 
 export const Navbar = () => {
 	return (
-		<div className="w-full h-20 bg-navbarBlue sticky top-0">
+		<div className="w-full h-[60px] sticky top-0 shadow-md">
 			<div className="container mx-auto px-4 h-full">
 				<div className="flex justify-between items-center h-full">
 					<Logo />
