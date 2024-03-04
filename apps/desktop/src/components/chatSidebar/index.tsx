e0df64912f8d2cb6d6c2050090dcd6ada2a53c8c
Heidar-An <heidaranwari@gmail.com>
2024-03-04 00:01:55 +0000

    feat: create shrink and expand button, show text when expanded

--- apps/desktop/src/components/chatSidebar/index.tsx
+++ apps/desktop/src/components/chatSidebar/index.tsx
@@ -1,21 +1,53 @@
-// import { ChatButton } from "@/components/chatButton";
 import { CircularChatButton } from "@/components/circularChatButton";
-import { faker } from "@faker-js/faker";
+import React, { useState } from "react";
 
 function getChatNames(): string[] {
 	// return new Array(15).fill(true).map(() => faker.lorem.sentence({ min: 10, max: 30 }));
-	return ["David", "Group Chat!", "random", "another server", "my server", "friends"];
+	// return ["David", "Group Chat!", "random", "another server", "my server", "friends"];
+	return [
+		"David",
+		"Group Chat!",
+		"random",
+		"another server",
+		"my server",
+		"friends",
+		"Slack",
+		"Discord",
+		"College",
+		"McDonalds",
+		"RUSTT",
+	];
 }
 
 export default function ChatSidebar() {
+	const [isExpanded, setIsExpanded] = useState(false);
+
+	const toggleExpansion = () => {
+		setIsExpanded(!isExpanded);
+	};
+
 	const chatNames = getChatNames();
+
+	const align = isExpanded ? "items-start ml-3" : "items-center";
+	
 	return (
 		// "no-scrollbar" defined in globals.css
-		<div className="flex flex-col items-center no-scrollbar overflow-auto space-y-4 bg-darkGrey h-full w-20 pt-2 pb-2"> 
-			{chatNames.map((chatName, index) => (
-				<CircularChatButton key={index} chatName={chatName} />
-			))}
-			<CircularChatButton chatName = "+"/>
+		<div className={`h-full ${isExpanded ? "min-w-70" : "min-w-20"}`}>
+			<div className="flex flex-col justify-between bg-darkGrey pt-2 pb-2 h-full">
+				<div className="flex flex-col items-center pt-2">
+					<button onClick={toggleExpansion} className="mt-auto mb-2 text-white font-bold text-2xl">
+						{isExpanded ? "-" : "+"}
+					</button>
+				</div>
+				<div className={`flex flex-col ${align} no-scrollbar overflow-auto space-y-4`}>
+					{chatNames.map((chatName, index) => (
+						<CircularChatButton key={index} chatName={chatName} isExpanded={isExpanded} />
+					))}
+				</div>
+				<div className={`flex flex-col items-start pt-2 ${align}`}>
+					<CircularChatButton chatName="+" isExpanded={isExpanded} />
+				</div>
+			</div>
 		</div>
 	);
 }
--- apps/desktop/src/components/circularChatButton/index.tsx
+++ apps/desktop/src/components/circularChatButton/index.tsx
@@ -1,31 +1,45 @@
 import Link from "next/link";
 import React from "react";
+
 import { ChatTooltip } from "./tooltip";
 
-export const CircularChatButton: React.FC<{chatName: string}> = ({chatName}) => {
+export const CircularChatButton: React.FC<{ chatName: string; isExpanded: boolean }> = ({
+	chatName,
+	isExpanded,
+}) => {
 	// specific case for new chat button (because it has a different colour)
-	if (chatName == "+"){
+	if (chatName == "+") {
 		return (
 			<div>
-				<Link href="./newChat" className="flex items-center justify-center min-h-14 w-14 h-14 rounded-xl bg-lightGrey hover:bg-discordGreen text-discordGreen hover:text-white text-4xl">
+				<Link
+					href="./newChat"
+					className="flex items-center justify-center min-h-14 w-14 h-14 rounded-xl bg-lightGrey hover:bg-discordGreen text-discordGreen hover:text-white text-4xl"
+				>
 					+
 				</Link>
 				{/* <ChatTooltip tooltip={chatName}/> */}
 			</div>
-		)
-	}
-	else{
+		);
+	} else {
 		// get first three words in chat name
 		let chatNameInWords = chatName.split(" ", 3);
-		let innerText = chatNameInWords.map(word => 
-			word.charAt(0)).join("")
+		// then get the first letter from these words
+		let innerText = chatNameInWords.map((word) => word.charAt(0)).join("");
+
+		let serverName = chatName.length >= 10 ? chatName.slice(0, 8) + "...": chatName.slice(0, 8);
 		return (
-			<div className="relative group">
-				<Link href={chatName} className="flex items-center justify-center w-14 h-14 min-h-14 rounded-xl bg-lightGrey hover:bg-hoverLightGrey text-white text-xl">
+			<div className={`flex items-center ${isExpanded ? "min-w-50": "min-w-14"}`}>
+				<Link
+					href={chatName}
+					className="flex items-center justify-center min-w-14 h-14 min-h-14 rounded-xl bg-lightGrey hover:bg-hoverLightGrey text-white text-xl"
+				>
 					{innerText}
 				</Link>
+				{isExpanded && (
+					<span className="text-white font-semibold ml-3 mr-2 overflow-visible whitespace-pre">{serverName}</span>
+				)}
 				{/* <ChatTooltip tooltip={chatName}/> */}
 			</div>
-		)
+		);
 	}
-}
+};
