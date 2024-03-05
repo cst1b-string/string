ee0bce2a78569f3ccd35f7f01b1555f349f783a5
Amaan <ah2254@cam.ac.uk>
2024-03-05 16:49:59 +0000

    feat: sends a message on keypress, waiting for login

--- apps/desktop/src/app/page.tsx
+++ apps/desktop/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import ChatSidebar from "@/components/chatSidebar";
 import ChatLog from "@/components/chatlog";
+import { useRspc } from "@/integration";
 import { redirect } from "next/navigation";
 import { createContext, useState } from "react";
 
@@ -14,11 +15,22 @@ var hasAccount = true;
 
 export default function Home() {
 	const [selectedChannel, setSelectedChannel] = useState(-1);
+	const [inputValue, setInputValue] = useState("");
 
 	if (!hasAccount) {
 		redirect("/signUp");
 	}
 	const [lightMode, setLightMode] = useState(false);
+	const rspc = useRspc();
+	const sendMsg = rspc.useMutation("channel.send");
+
+	const keyDown = (event) => {
+		console.log("pressed something");
+		if (inputValue.length > 0 && event.key == "Enter") {
+			sendMsg.mutate({ channel_id: selectedChannel, content: inputValue });
+			console.log("pressed enter");
+		}
+	};
 
 	return (
 		<div className="grid grid-cols-[auto,1fr] h-[calc(100vh-60px)]">
@@ -26,9 +38,12 @@ export default function Home() {
 			<div className="grid grid-rows-[1fr,auto] h-full">
 				<ChatLog selectedChannel={selectedChannel} />
 				<div className="px-2 py-2 min-h-16">
-					<textarea 
+					<textarea
+						value={inputValue}
+						onKeyDown={keyDown}
 						placeholder="Type a message here..."
-						className="px-1 h-16 w-full input rounded bg-darkSidebar text-darkText" 
+						className="px-1 h-16 w-full input rounded bg-darkSidebar text-darkText"
+						onChange={(e) => setInputValue(e.target.value)}
 					/>
 				</div>
 			</div>
