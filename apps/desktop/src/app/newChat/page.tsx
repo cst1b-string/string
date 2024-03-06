de732278845bbf183b28af5b6f2c3a57621e5bed
David Murray <dm966@cam.ac.uk>
2024-03-06 15:19:30 +0000

    feat: colour changes for new chat page

--- apps/desktop/src/app/newChat/page.tsx
+++ apps/desktop/src/app/newChat/page.tsx
@@ -20,7 +20,7 @@ export default function NewChat() {
 
 	return (
 		<div className="py-6 px-4 flex justify-center space-y-2">
-			<div className="bg-formBlue text-white rounded px-12 py-10 grid divide-y divide-gray-400 space-y-10 w-[600px] l:w-[700px] xl:w-[850px]">
+			<div className="bg-darkForm text-darkText rounded px-12 py-10 grid divide-y divide-gray-400 space-y-10 w-[600px] l:w-[700px] xl:w-[850px]">
 				<div className="">
 					<h1 className="text-2xl">Create New Chat</h1>
 					<br />
@@ -34,12 +34,12 @@ export default function NewChat() {
 								required
 								maxLength={30}
 								type="text"
-								className="py-1 px-1 rounded bg-darkInput w-full"
+								className="py-1 px-1 rounded bg-darkSidebar w-full"
 							/>
 						</label>
 						<button
 							type="submit"
-							className="py-2 rounded drop-shadow-lg hover:bg-darkHover bg-darkInput text-white"
+							className="py-2 rounded drop-shadow-lg hover:bg-darkHover bg-darkSidebar text-white"
 						>
 							Create
 						</button>
@@ -53,12 +53,12 @@ export default function NewChat() {
 						<label>
 							Chat Link
 							<br />
-							<input type="text" className="py-1 px-1 rounded bg-buttonBlue w-full" />
+							<input type="text" className="py-1 px-1 rounded bg-darkSidebar w-full" />
 						</label>
 						<br />
 						<button
 							type="submit"
-							className="py-2 rounded drop-shadow-lg hover:bg-hoverBlue bg-buttonBlue text-white"
+							className="py-2 rounded drop-shadow-lg hover:bg-darkHover bg-darkSidebar text-white"
 						>
 							Join
 						</button>
